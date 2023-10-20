575114f5550a09fda35971deec79d22903b552df
Adriano Ivan de Paula Campos <adrianoivandepaulacampos@gmail.com>
2023-10-20 08:03:51 -0300

    .next in .gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 function Home() {
-    return <h1>Aqui começa a construção de um sistema gigante !</h1>
+    return <h1>Aqui começa a construção de um sistema gigante ! teste</h1>
 }
 
 export default Home;
